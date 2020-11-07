f45a4deaa59fda51b5a641e14a9aa07b3039bf63
Gabriel <laurencio.arkauss@gmail.com>
2020-11-07 16:12:04 -0300

    feat (queue.page, auth.service) : done the interaction of the queue page

--- src/app/queue-page/queue-page.component.ts
+++ src/app/queue-page/queue-page.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+
+interface User{
+  id:any,
+  nickname:string
+}
 
 @Component({
   selector: 'app-queue-page',
@@ -7,11 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueuePageComponent implements OnInit {
 
-  constructor() { }
+  userLogged : any ;
+  users : User[] = [
+    {
+      id:'1joel',
+      nickname:'Joel'
+  },{
+    id:'2Gigante',
+    nickname:'Gigante'
+  },{
+    id:'3Ariane',
+    nickname:'Ariane'
+  }];
 
+  constructor(private authService : AuthService, private router : Router) {
+    if(this.authService.getUser() == 'null' || !JSON.parse(this.authService.getUser()).id ){
+      this.router.navigateByUrl("");
+    }
+    this.userLogged = JSON.parse(this.authService.getUser());
+   }
+  
   ngOnInit(): void {
+    // quando tiver back end vai ser aqui que iremos consumir o end point que carrega os usuarios que estão na fila
   }
-  getinQueue(){
-    
+
+  enterQueue(){
+    this.users.push(this.userLogged);
   }
+
+  leaveQueue(){
+    if(!this.userLogged){
+      return
+    }
+    this.users = this.users.filter(user => user.id != this.userLogged.id);
+  }
+
+  userIsQueue(){
+    return this.users.find(user => this.userLogged && user.id == this.userLogged.id);
+  }
+
+  getRandomNumber(){
+    return Math.floor(Math.random() * 888 + 1)
+  }
+
+  logout(){
+    this.authService.setUser(null);
+    this.router.navigateByUrl("");
+  }
+
+  
 }
