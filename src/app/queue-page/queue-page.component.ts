d380efa8a3dafdc979b56714f0c1a39fd2518401
Ariane Leite <arileitte@gmail.com>
2020-11-07 15:18:50 -0300

    fix : Resolved merge conflict.

--- src/app/queue-page/queue-page.component.ts
+++ src/app/queue-page/queue-page.component.ts
@@ -1,36 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-var fila = [];
-fila.push[1]
-=======
->>>>>>> 3ce13c5fb82cf56708a26272dec02a27307ed1ef
 
 @Component({
   selector: 'app-queue-page',
   templateUrl: './queue-page.component.html',
   styleUrls: ['./queue-page.component.scss']
 })
-<<<<<<< HEAD
-
-=======
->>>>>>> 3ce13c5fb82cf56708a26272dec02a27307ed1ef
 export class QueuePageComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-<<<<<<< HEAD
-  getinQueue() {
-
-    var last = fila.length-1;
-    var current = last +1;
-    fila.push(current);
-    console.log(fila);
-
+  getinQueue(){
+    
   }
-
-=======
->>>>>>> 3ce13c5fb82cf56708a26272dec02a27307ed1ef
 }
