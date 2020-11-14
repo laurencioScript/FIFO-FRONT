370902f3b8a75a1aecd43805160875f33acb2927
Gabriel <laurencio.arkauss@gmail.com>
2020-11-14 18:56:05 -0300

    feat : connecting to the back end and logging in

--- src/app/home-page/form-login/form-login.component.ts
+++ src/app/home-page/form-login/form-login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-form-login',
@@ -11,23 +13,40 @@ import { AuthService } from 'src/app/service/auth.service';
 export class FormLoginComponent implements OnInit {
 
   nickName : string = '';
+  users : any = [];
 
-  constructor(private readonly authService : AuthService, 
+  constructor(private readonly authService : AuthService,
+    private readonly userService : UserService, 
     private router : Router,
-    public dialogRef: MatDialogRef<FormLoginComponent>) { }
+    public dialogRef: MatDialogRef<FormLoginComponent>,
+    private _snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {
+  async ngOnInit()  {
+    this.users = await this.userService.getUsers();
   }
 
   login(){
+    console.log('>>> this.users',this.users);
+    console.log('>>> this.nickName',this.nickName);
+    const existUser = this.users.find(user => user.nickname == this.nickName);
 
-    this.authService.setUser(JSON.stringify({
-      nickname:this.nickName,
-      id:this.generateId()
-    })) ;
+    if(!existUser){
+      this.openSnackBar('Nickname invalido','ok','snack-bar-danger');
+      return
+    }
+
+    this.authService.setUser(JSON.stringify(existUser)) ;
     this.dialogRef.close();
     this.router.navigateByUrl("filas");
+  }
 
+  openSnackBar(message: string, action: string, color) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+      verticalPosition: "top",
+      horizontalPosition: "center",
+      panelClass: color,
+    });
   }
 
   generateId(){
